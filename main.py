04216993080524095f4ecccefd4c328b0863d801
absiddik7 <abubakkar1070@gmail.com>
2024-03-14 13:49:17 +0600

    framework updated

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     update_headless_mode(headless)
 
     test_files = [
-        "src/tests/test_login.py",
+        "src/tests/login/test_login.py",
     ]
 
     main()
