3bda26c3001ec593eea9fe2498e0252eba7cfad7
absiddik7 <abubakkar1070@gmail.com>
2024-03-20 14:22:33 +0600

    test product updated

--- src/pages/catalog/products_page.py
+++ src/pages/catalog/products_page.py
@@ -1,3 +1,4 @@
+from tkinter import S
 from selenium.webdriver.common.by import By
 from src.utils.urls import URLs
 from selenium.webdriver.support.ui import Select
@@ -9,29 +10,32 @@ class ProductsPage:
         self.driver = driver_manager.get_driver()
 
         # Page elements
+        # Navigation drawer elements
         self.navigation_drawer_catalog_element = (By.XPATH, "//li[contains(@class, 'nav-item') and .//p[contains(., 'Catalog')]]")
         self.catelog_dropdown_products_element = (By.XPATH, "//a[@href='/Admin/Product/List']")
+        # Products search page elements
         self.add_new_button = (By.CSS_SELECTOR, 'a[href="/Admin/Product/Create"]')
-        self.product_info_form = (By.XPATH, "//div[@id='product-info']//button[@data-card-widget='collapse']")
-        self.product_price_form = (By.XPATH, "//div[@id='product-price']//button[@data-card-widget='collapse']")
         self.product_name_search_input = (By.CSS_SELECTOR, "input#SearchProductName")
-        self.category_select_element = (By.ID, 'SelectedCategoryIds')
+        self.category_select_element = (By.CSS_SELECTOR, '#SearchCategoryId')
         self.search_button = (By.CSS_SELECTOR, "button#search-products")
+        # add new product page elements
+        self.product_info_form = (By.XPATH, "//*[@id='product-info']")
+        self.product_price_form = (By.XPATH, "//div[@id='product-price']//button[@data-card-widget='collapse']")
         self.product_name_input = (By.CSS_SELECTOR, "input#Name")
         self.short_description_input = (By.CSS_SELECTOR, "textarea#ShortDescription")
-
-        self.category_select = (By.CSS_SELECTOR, "input[aria-describedby='SelectedCategoryIds_taglist']")
+        self.category_select = (By.CSS_SELECTOR, "#product-info > div.card-body > div:nth-child(2) > div.col-md-9 > div > div > input")
         self.category_options = (By.TAG_NAME, "option")
         self.sku_input = (By.XPATH, "//input[@id='Sku']")
-        self.price_input = (By.XPATH, "//input[@class='k-formatted-value k-input']")
-        self.save_button = (By.XPATH, "//button[@class='btn btn-primary']")
-        self.save_and_continue_edit_button = (By.XPATH, "//button[@class='btn btn-primary' and contains(text(), 'Save and Continue Edit')]")
+        self.price_input = (By.CSS_SELECTOR, "#product-price-area > div.col-md-9 > span.k-widget.k-numerictextbox > span > input.k-formatted-value.k-input")
+        self.save_button = (By.CSS_SELECTOR, "#product-form > div.content-header.clearfix > div > button:nth-child(1)")
+        self.save_and_continue_edit_button = (By.CSS_SELECTOR, "#product-form > div.content-header.clearfix > div > button:nth-child(2)")
         self.multimedia_card = (By.ID, "product-multimedia")
         self.upload_button = (By.CLASS_NAME, "qq-upload-button-selector")
         self.file_input = (By.XPATH, "//input[@type='file']")
         self.edit_button = (By.CSS_SELECTOR, "a.btn.btn-default[href*='Edit/']")
         self.delete_button = (By.ID, "product-delete")
         self.search_result_row = (By.XPATH, "//tbody//tr")
+        self.success_notification = (By.CSS_SELECTOR, "body > div.wrapper > div.content-wrapper > div.alert.alert-success.alert-dismissable")
 
 
     # navigate to products page
@@ -43,6 +47,7 @@ class ProductsPage:
     # search for a product
     def search_for_product(self, product_name, category):
         self.driver.find_element(*self.product_name_search_input).send_keys(product_name)
+        self.driver.find_element(*self.category_select_element).click()
         Select(self.driver.find_element(*self.category_select_element)).select_by_visible_text(category)
         self.driver.find_element(*self.search_button).click()
 
@@ -64,6 +69,8 @@ class ProductsPage:
             print("Search result does not match the expected values.")
             return False 
 
+    
+
     # Click the "Add New" button to start adding a new product
     def click_add_new_button(self):
         self.driver.find_element(*self.add_new_button).click()
@@ -82,9 +89,14 @@ class ProductsPage:
         self.driver.find_element(*self.short_description_input).send_keys(short_description)
         self.driver.find_element(*self.sku_input).send_keys(sku)
         self.driver.find_element(*self.category_select).click()
-        Select(self.driver.find_element(*self.category_select)).select_by_visible_text(category)
-        
+        list_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.ID, "SelectedCategoryIds_listbox"))
+        )
+        # Find the option with text "Computers" and click on it
+        option = list_element.find_element(By.XPATH, f"//li[text()= ${category}]")
+        option.click()
         self.driver.find_element(*self.price_input).send_keys(price)
+
 
     # add product image
     def add_product_image(self, image_path):
@@ -104,13 +116,20 @@ class ProductsPage:
     def add_new_product(self, product_name, short_description, category, sku, price, image_path):
         self.click_add_new_button()
         self.add_product_details(product_name, short_description, category, sku, price)
-        #self.add_product_image(image_path)
         self.save_and_continue_edit()
+        WebDriverWait(self.driver, 5)
+        self.add_product_image(image_path)
+        WebDriverWait(self.driver, 5)
         self.save_product()
+        
 
     # validate product added
-    def validate_product_added(self, expected_product_name, expected_sku, expected_price):
+    def validate_product_added(self, expected_product_name, expected_category, expected_sku, expected_price):
+        WebDriverWait(self.driver, 10)
+        self.driver.find_element(*self.success_notification).is_displayed()
         # Search for the added product
+        self.search_for_product(expected_product_name, expected_category)
+        WebDriverWait(self.driver, 5)
         if self.validate_search_result(expected_product_name, expected_sku, expected_price):
             print("Product was successfully added.")
             return True
--- src/tests/catalog/test_products.py
+++ src/tests/catalog/test_products.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import json
 from src.pages.login.login_page import LoginPage
@@ -25,13 +26,22 @@ def login(driver_manager, login_data):
     login_page = LoginPage(driver_manager)
     login_page.login(login_data['valid_credentials']['admin']['username'], login_data['valid_credentials']['admin']['password'])
 
+
+# def test_search_for_product(driver_manager, login, product_data):
+#     products_page = ProductsPage(driver_manager)
+#     products_page.navigate_to_products_page()
+#     product = product_data['products']['existing_products'][0]
+#     products_page.search_for_product(product['name'], product['category'])
+#     assert products_page.validate_search_result(product['name'], product['sku'], product['price'])
+
 def test_add_new_product(driver_manager, login, product_data):
     products_page = ProductsPage(driver_manager)
     products_page.navigate_to_products_page()
     new_product = product_data['products']['new_products'][0]
+    image_path = os.path.abspath(new_product['image_path'])
     products_page.add_new_product(new_product['name'], new_product['description'], new_product['category'],
-                                   new_product['sku'], new_product['price'], new_product['image_path'])
-    assert products_page.validate_product_added(new_product['name'], new_product['sku'], new_product['price'])
+                                   new_product['sku'], new_product['price'], image_path)
+    assert products_page.validate_product_added(new_product['name'], new_product['category'], new_product['sku'], new_product['price'])
 
 # def test_edit_existing_product(driver_manager, login, product_data):
 #     products_page = ProductsPage(driver_manager)
